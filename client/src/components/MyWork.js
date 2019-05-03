ce9cea413b1bee603a04e9b6b0913a013fb58416
Andre Campbell <andrecampbell@Andres-MacBook-Pro.local>
2019-05-03 12:17:32 -0600

    Adding to my work tweaking styles

---
+++ client/src/components/MyWork.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const MyWork = () => {
+    return(
+        <div>
+            <h3>My Work</h3>
+            <h5>Here is a list of my Notable projects and the technology that is being used!</h5>
+
+            <div className= 'container'>
+                <div className= 'row'>
+                    <div className= 'col-md-4'>
+
+                    </div>
+                    <div className= 'col-md-4'>
+                        <p> The Closet a React / Express app <br/>
+                             The Closet makes it easy to curate a style that's 100% you effortlessly! <br/>
+                             I worked with two other collegues on this project.<br/>
+                             My work included 
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MyWork;
