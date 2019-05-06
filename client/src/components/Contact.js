028c52faff2577d4536f2db1854c32528da4a356
Andre Campbell <andrecampbell@Andres-MacBook-Pro.local>
2019-05-06 13:50:57 -0600

    Changed styling on contact

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -17,35 +17,35 @@ function Header() {
                     <h4 style={{ color: 'white' }}>Click on the Links to Contact Me!</h4>
                     <div className='col-md-12'>
                         <div className='row'>
-                            <div className='col-md-2' />
-                            <div className="card col-md-3" style={{ width: "16rem", height: '100px', margin: '10px', backgroundColor: '#414099' }}>
+                            <div className='col-md-1' />
+                            <div className="card col-md-4" style={{ width: "16rem", height: '100px', margin: '10px', backgroundColor: '#414099' }}>
                                 <div className="card-body">
-                                    <h6 className="card-text"><h4><strong>Email</strong></h4> <a href='mailto:andre2656@gmail.com' style={{ color: 'yellow', fontWeight: 'bold'}}> andre2656@gmail.com</a></h6>
+                                    <h6 className="card-text"><h4><strong>Email</strong></h4> <a href='mailto:andre2656@gmail.com' style={{ color: 'white', fontWeight: 'bold'}}> andre2656@gmail.com</a></h6>
                                 </div>
                             </div>
                             <div className='col-md-1' />
-                            <div className="card col-md-3" style={{ width: "16rem", height: '100px', margin: '10px', backgroundColor: '#00755c' }}>
+                            <div className="card col-md-4" style={{ width: "16rem", height: '100px', margin: '10px', backgroundColor: '#00755c' }}>
                                 
                                 <div className="card-body">
-                                    <h6 className="card-text"><h4><strong>Phone</strong></h4> <a href='#' style={{ color: '#00addc', fontWeight: 'bold' }}> (714) 803-2687</a></h6>
-                                </div>
-                            </div>
-                            <div className='col-md-3' />
-                           </div>
-                           <div className= 'row'> 
-                            <div className='col-md-3' />
-                            <div className="card col-md-3" style={{ width: "16rem", margin: '10px', height: '100px', backgroundColor: '#ef4123' }}>
-                                <div className="card-body">
-                                    <h6 className="card-text"><h4><strong>Github</strong></h4> <a href='https://github.com/andre2656' target='_blank' style={{ color: '#ffffff', fontWeight: 'bold' }}>andre2656</a></h6>
-                                </div>
-                            </div>
-                            <div className='col-md-1' />
-                            <div className="card col-md-3" style={{ width: "16rem", margin: '10px', height: '100px', backgroundColor: '#00addc' }}>
-                                <div className="card-body">
-                                    <h6 className="card-text"><h4><strong>LinkedIn</strong></h4><a href='https://www.linkedin.com/in/andrecampbell-dev/' target='_blank' style={{ color: 'yellow', fontWeight: 'bold' }}> Andre Campbell</a></h6>
+                                    <h6 className="card-text"><h4><strong>Phone</strong></h4> <a href='#' style={{ color: 'white', fontWeight: 'bold' }}> (714) 803-2687</a></h6>
                                 </div>
                             </div>
                             <div className='col-md-2' />
+                           </div>
+                           <div className= 'row'> 
+                            <div className='col-md-2' />
+                            <div className="card col-md-4" style={{ width: "16rem", margin: '10px', height: '100px', backgroundColor: '#ef4123' }}>
+                                <div className="card-body">
+                                    <h6 className="card-text"><h4><strong>Github</strong></h4> <a href='https://github.com/andre2656' target='_blank' style={{ color: 'white', fontWeight: 'bold' }}>andre2656</a></h6>
+                                </div>
+                            </div>
+                            <div className='col-md-1' />
+                            <div className="card col-md-4" style={{ width: "16rem", margin: '10px', height: '100px', backgroundColor: '#00addc' }}>
+                                <div className="card-body">
+                                    <h6 className="card-text"><h4><strong>LinkedIn</strong></h4><a href='https://www.linkedin.com/in/andrecampbell-dev/' target='_blank' style={{ color: 'white', fontWeight: 'bold' }}> Andre Campbell</a></h6>
+                                </div>
+                            </div>
+                            <div className='col-md-1' />
                         </div>
                     </div>
                 </div>
